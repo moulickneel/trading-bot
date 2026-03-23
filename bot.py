551e4ece9255c2f3722e9370102dbddb959f576b
moulickneel <moulick.neel@gmail.com>
2026-03-23 19:55:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    print("Signal received:", data)
+
+    if data["action"] == "buy":
+        print("BUY SIGNAL")
+
+    if data["action"] == "sell":
+        print("SELL SIGNAL")
+
+    return "ok"
+
+app.run(host="0.0.0.0", port=10000)
